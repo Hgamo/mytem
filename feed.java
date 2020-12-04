15aa452bcb68f95e904c19d5f47b31ca7092711c
Hgamo <73846452+Hgamo@users.noreply.github.com>
2020-12-04 13:57:43 +0100

    add the first classes

---
+++ feed.java
@@ -0,0 +1,2 @@
+class Feed extends List<FeedItem>{
+} 
---
+++ feed_item.java
@@ -0,0 +1,12 @@
+class FeedItem{
+/**
+* Drei Atribute werden deklariert:
+* 	Zeit (time)
+* 	Link zu dem Bild (src)
+* 	Beschreibung (description)
+* ^vst
+*/
+	private Date time;
+	private string src; 
+	
+}
