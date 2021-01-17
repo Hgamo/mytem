3b29c63e5b1faaf9ff8310d4de4733339220b701
4pixx <45511517+4pixx@users.noreply.github.com>
2021-01-17 16:09:03 +0100

    Update Feed.java
    Updated SuchFunktion

--- Feed.java
+++ Feed.java
@@ -38,12 +38,12 @@ public class Feed extends List<FeedItem> {
     while (current != null){ 
       if (current.time == gesucht) {
         return true;
-        System.out.println("Es wurde ein Element mit der Zeit " gesucht " in Liste gefunden");}
+        System.out.println("Es wurde ein Element mit der Zeit " gesucht " am Knoten " current " mit der ID " current.id " gefunden.");}
       else{
         current = current.next; }
       } 
       return false;
-      System.out.println("Es wurde KEIN (weiteres) Element mit der Zeit " gesucht " in Liste gefunden");}
+      System.out.println("Es wurde KEIN (weiteres) Element mit der Zeit " gesucht " in Liste gefunden");
   }
   
   public void suchFunktionPerID (int gesucht) {
@@ -52,12 +52,12 @@ public class Feed extends List<FeedItem> {
     while (current != null){ 
       if (current.id == gesucht) {
         return true;
-        System.out.println("Es wurde ein Element mit der ID " gesucht " in Liste gefunden");}
+        System.out.println("Es wurde ein Element mit der ID " gesucht " am Knoten " current " in Liste gefunden");}
       else{
         current = current.next; }
       } 
       return false;
-      System.out.println("Es wurde KEIN (weiteres) Element mit der ID " gesucht " in Liste gefunden");}
+      System.out.println("Es wurde KEIN (weiteres) Element mit der ID " gesucht " in Liste gefunden");
   }
 
   
